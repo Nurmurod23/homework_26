e229fa3f7951fcfeb6ce7d117a136f386f629e80
Nurmurod <elow9020@gmail.com>
2023-10-09 04:18:04 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,367 @@
+//if_1
+
+// var A = parseInt(prompt("Xoxlagan bir son kiriting"));
+
+// if(A > 0){
+//     alert(A+1);
+// }else(false)
+
+
+
+//if_2
+
+// const A = parseInt(prompt("Xoxlagan son kiriting"));
+
+// if(A>0){
+//     alert(A+1)
+// }else{
+//     alert(A-2)
+// }
+
+
+//if_3
+
+// var A = parseInt(prompt("Xoxlagan son"))
+
+
+// if (A>0){
+//             alert(A+1);
+// }else if (A == 0){
+//          alert(A + 10);
+// }else if(A < 0){
+//             alert(A + 2)
+// }
+
+
+//if_4
+
+// let A,X=0;
+// for(let 
+//     i=0;
+//     i<1; 
+//     i++)
+// {
+//     A = parseFloat(prompt("Xoxlagan son kiriting"));
+//     if( A>0 ) {
+//          X++ 
+//     };
+// }
+// alert(X);
+
+
+//if_6
+
+// let A = parseInt(prompt("A uchun xoxlagan son kiriting"));
+// let B = parseInt(prompt("B uchun xoxlagan son kiriting"));
+
+// if (A>B){
+//     alert(`Eng katta son: ${A}`);
+    
+// }else{
+//     alert(`Eng katta son: ${B}`);
+// }
+
+
+//if_7
+
+// let A = parseInt(prompt("A uchun xoxlagan son kiriting"));
+// let B = parseInt(prompt("B uchun xoxlagan son kiriting"));
+
+// if (A<B){
+//     alert(`Eng kichik son: ${A}`);
+    
+// }else{
+//     alert(`Eng kichik son: ${B}`);
+// }
+
+//if_8
+
+// let A = parseInt(prompt("A uchun xoxlagan son kiriting"));
+// let B = parseInt(prompt("B uchun xoxlagan son kiriting"));
+
+// if (A>B){
+//     alert(`Eng katta son: ${A}`);
+//     alert(`Eng kichik son: ${B}`);
+
+    
+// }else{
+//     alert(`Eng katta son: ${A}`);
+//     alert(`Eng kichik son: ${B}`);
+// }
+
+
+
+//if_9
+
+// var A = parseInt(prompt("Xoxlagan son kiriting"));
+
+// var B = parseInt(prompt("Xoxlagan son kiriting"));
+
+// if(A > B) {
+//     alert(A - B)
+//     alert(B + A)
+// }else {
+//     alert(A)
+//     alert(B)
+// }
+
+
+
+//if_10
+
+// var a = prompt("Butun son kiriting: ")
+// var b= prompt("Butun son kiriting: ")
+
+// if(a===b){
+//     a = a+b;Math.floor
+//     b = a+b; Math.floor
+// }else{
+//     a=0
+//     b=0
+// }
+// alert(`${a}, ${b}`)
+
+
+//if_11
+
+
+
+
+// If-11
+
+// let a = parseInt(prompt("Enter the value of a"))
+// let b = parseInt(prompt("Enter the value of b"))
+
+// let result;
+// if ((a !== b && a > b)) {
+//     result = a
+//     alert(result)
+// }else if (a !== b && a < b) {
+//     result = b;
+//     alert(b)
+// }else if(a === b) {
+//     result = 0
+//     alert(result)
+// }
+
+
+
+
+
+
+// If-12
+
+// let a = 5;
+// let b = 3;
+// let c = 86;
+
+
+// if(a < b && a < c ) {
+//     alert(a)
+// }else if(b<a && b < c) {
+//     alert(b)
+// }else {
+//     alert(c)
+// }
+
+
+
+
+
+
+
+
+
+// If - 13
+
+// let a = 4;
+// let b = 9;
+// let c = 2;
+// let greatest;
+// let smallest;
+
+// if((a > b && a > c)) {
+//     greatest = a
+// }else if (b > 0 && b > c) {
+//     greatest = b
+// }else {
+//     greatest = c
+// }
+
+// if(a < b && a < c ) {
+//     smallest = a
+// }else if(b<a && b < c) {
+//     smallest = b
+// }else {
+//     smallest = c
+// }
+
+// alert(Math.floor((greatest + smallest)/2))
+
+
+
+
+
+
+
+//If - 14
+// let a = 4;
+// let b = 9;
+// let c = 2;
+
+// if(a < b && a < c ) {
+//     alert(a)
+// }else if(b<a && b < c) {
+//     alert(b)
+// }else {
+//     alert(c)
+// }
+// if((a > b && a > c)) {
+//     alert(a)
+// }else if (b > 0 && b > c) {
+//     alert(b)
+// }else {
+//     alert(c)
+// }
+
+
+
+
+
+//If - 15
+// let a = 4;
+// let b = 1;
+// let c = 2;
+
+// if((a>b && a>c) && (a>b &&b>c) &&(c<a && c<b)) {
+//     alert(a+b)
+// }else if((b>a && b>c) && (b>a &&a>c) &&(c<a && c<b)) {
+//     alert(b+a)
+// }else if((c>b && c>a) && (c>b &&b>c) &&(a<c && a <b )) {
+//     alert(c+b)
+// }else if((a>b && a>c)&& (a > c && c > b)&& (b<a && b<c)) {
+//     alert(a+c)
+// }
+
+
+
+
+//if is end
+
+//Boolean_1
+
+// var A = parseInt(prompt("Xoxlagan son kiriting"));
+
+// if (A > 0){
+//     alert(`"Berilgan son musbat " ${A}`)
+// }else{
+//     alert(false)
+// }
+
+
+
+//boolean_2
+
+// var A = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if (A % 2 == 1) {
+//     alert("Berilgan son toq son" );
+// } else {
+//     alert("Berilgan son juft");
+// }
+
+
+//boolean_3
+
+
+
+// var A = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if (A % 2 == 0) {
+//     alert("Berilgan son jusft son");
+// } else {
+//     alert("Berilgan son toq");
+// }
+
+
+
+//Boolean 4
+
+// let a = parseInt(prompt("Xoxlagan son kiriting"))
+// let b = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if(a > 2 && b <= 3) {
+//     alert(true)
+// }else {
+//     alert(false)
+// }
+
+
+
+// Boolean 5
+// let a = parseInt(prompt("Xoxlagan son kiriting"))
+// let b = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if(a >= 0 || b < -2) {
+//     alert(true)
+// }else {
+//     alert(false)
+// }
+
+
+
+
+
+//Boolean 6
+
+// let a = parseInt(prompt("Xoxlagan son kiriting"))
+// let b = parseInt(prompt("Xoxlagan son kiriting"))
+// let c = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if(b >=a && b <= c) {
+//     alert(true)
+// }else {
+//     alert(false)
+// }
+
+
+
+
+//Boolean 7
+
+// var a = parseInt(prompt("Xoxlagan son kiriting"))
+// var b = parseInt(prompt("Xoxlagan son kiriting"))
+// var c = parseInt(prompt("Xoxlagan son kiriting"))
+
+// if(b >a && b < c) {
+//     alert(true)
+// }else {
+//     alert(false)
+// }
+
+
+// Boolean 8
+
+// var a = parseInt(prompt("Xoxlagan son kiriting"))
+// var b = parseInt(prompt("Xoxlagan son kiriting"))
+
+
+// if(a % 2 !== 0 && b % 2 !== 0) {
+//     alert(`${a} va ${b} Berilgan qiymat toq son`)
+// }else {
+//     alert(`${a} va ${b} Berilgan qiymat juft son`)
+// }
+
+
+
+// Boolean 9 
+
+// var a = parseInt(prompt("Xoxlagan son kiriting"))
+// var b = parseInt(prompt("Xoxlagan son kiriting"))
+
+
+// if(a % 2 !== 0 || b % 2 !== 0) {
+//     alert(`Toq sonlar`)
+// }else {
+//     alert(`${a} va ${b} Juft son`)
+// }
